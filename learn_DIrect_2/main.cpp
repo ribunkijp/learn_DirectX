b923b6f7d4046aeb43b42cb7b35e35d9fa082da4
ribunki <lwh1194@gmail.com>
2025-06-11 11:52:47 +0900

    window create

--- learn_DIrect_2/main.cpp
+++ learn_DIrect_2/main.cpp
@@ -1,15 +1,53 @@
+/*
+
+WinMain（アプリの開始点）
+└─ RegisterClass（ウィンドウクラスを登録）
+└─ CreateWindowEx（ウィンドウ作成）
+     └─ WM_CREATE メッセージが送られる（今回は処理なし）
+└─ ShowWindow / UpdateWindow（ウィンドウを表示）
+└─ メッセージループ開始（GetMessage → DispatchMessage）
+     └─ WindowProc（各メッセージごとの処理関数）
+          └─ WM_PAINT / WM_CLOSE / WM_DESTROY などの処理
+*/
+
+
+
 #ifndef UNICODE
 #define UNICODE
 #endif 
 
 #include <windows.h>
+#include <new>
 
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-{
+
+struct StateInfo {
+
+};
+
+
+inline StateInfo* GetAppState(HWND hwnd);
+
+
+LRESULT CALLBACK WindowProc(
+    HWND hwnd, 
+    UINT uMsg, 
+    WPARAM wParam, 
+    LPARAM lParam
+);
+
+//エントリー
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance, 
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ PWSTR pCmdLine,
+    _In_ int nCmdShow
+){
+    (void)hPrevInstance;// 使わないけど警告を消すために明示的に参照
+    (void)pCmdLine;//同上
+    
     // Register the window class.
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
@@ -21,12 +59,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     RegisterClass(&wc);
 
+
+    StateInfo* pState = new StateInfo();  // new で初期化
+
+    if (pState == NULL)
+    {
+        return 0;
+    }
+
+
+
     // Create the window.
 
     HWND hwnd = CreateWindowEx(
         0,                              // Optional window styles.
         CLASS_NAME,                     // Window class
-        L"Learn to Program Windows",    // Window text
+        L"Window_1",    // Window text  L -> UTF-16
         WS_OVERLAPPEDWINDOW,            // Window style
 
         // Size and position
@@ -35,7 +83,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL,       // Parent window    
         NULL,       // Menu
         hInstance,  // Instance handle
-        NULL        // Additional application data
+        pState        // Additional application data  ← ここが lpParam、WM_CREATE で受け取れる
     );
 
     if (hwnd == NULL)
@@ -57,10 +105,34 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     return 0;
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WindowProc(
+    HWND hwnd, 
+    UINT uMsg, 
+    WPARAM wParam, //UINT_PTR（符号なし整数）
+    LPARAM lParam)//LONG_PTR（符号付き整数）
 {
+
+    StateInfo* pState = nullptr;//StateInfo という構造体（構造体＝データまとめ）へのポインタをここに保存します。
+    
+    if (uMsg == WM_CREATE)
+    {
+        //CreateWindowExで渡した pStateを取り出し、ウィンドウに保存
+        CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+        pState = reinterpret_cast<StateInfo*>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pState);
+    }
+    else
+    {
+        pState = GetAppState(hwnd);
+    }
+
     switch (uMsg)
     {
+    case WM_CLOSE:
+        if (MessageBox(hwnd, L"really quit", L"cancel", MB_OKCANCEL) == IDOK) {
+            DestroyWindow(hwnd);
+        }
+        return 0;
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
@@ -80,4 +152,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+
+inline StateInfo* GetAppState(HWND hwnd)
+{
+    LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+    StateInfo* pState = reinterpret_cast<StateInfo*>(ptr);
+    return pState;
 }
